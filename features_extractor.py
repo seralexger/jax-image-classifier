b6abbbea9c1ed5ef845332080f9406961570be99
Alejandro <alex.german12@gmail.com>
2021-02-07 19:29:15 +0100

    README update

---
+++ features_extractor.py
@@ -0,0 +1,10 @@
+# You can implement any feature extractor, always taking in account the requirements for its input.
+
+def extract_VGG16(tensor):
+	from keras.applications.vgg16 import VGG16, preprocess_input
+	return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+def extract_VGG19(tensor):
+	from keras.applications.vgg19 import VGG19, preprocess_input
+	return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
